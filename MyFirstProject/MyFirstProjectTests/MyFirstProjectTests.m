8c87e449271453a8a84866f9242033fcc8e79eea
LoveToday <1215999200@qq.com>
2014-03-30 19:18:31 +0800

    chen

---
+++ MyFirstProject/MyFirstProjectTests/MyFirstProjectTests.m
@@ -0,0 +1,34 @@
+//
+//  MyFirstProjectTests.m
+//  MyFirstProjectTests
+//
+//  Created by lanou3g on 14-3-30.
+//  Copyright (c) 2014年 lanou3g. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyFirstProjectTests : XCTestCase
+
+@end
+
+@implementation MyFirstProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
